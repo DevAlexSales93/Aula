fd35a040cbaa13274ae1f0b24d49580fccee9451
ALEX SALES DE SOUZA <alex.ssouza20@senacsp.edu.br>
2026-07-21 20:20:06 -0300

    calculando a idade

--- introducao_funcao.py
+++ introducao_funcao.py
@@ -1,11 +1,28 @@
 # aqui criaremos nossas proprias funções. sintaxe.
 # def nome_funcao():
 
-def menssagem_boas_vindas():
+def mensagem_boas_vindas():
     # dentro de uma função e colocado o que chamamos de bloco de códigos, que são códigos identados. ELES SERAM EXECUTADOS.
-    print("Seja bem-vindo ao sistema DEV SOCIEDADE ANONIMA")
+    print("Seja bem-vindo ao sistema DEV | SOCIEDADE ANONIMA")
     print("Sua principal ferramenta de desenvolvimento anonima.")
-    print("-"*10)
-    print("Siga o MENU ABAIXO: ⤵")
+    print("-"*55)
+    print("Siga o MENU ABAIXO: |⤵|")
+# ------------------------------------------------------
+def cantar_musica():
+    print("Barretos quando entra em campo a torcida vibra com emoção e tudo isto é ternura é uma paixão que é pura vem do coração.")
+    print("Salve o Barretos, time do peito com o Barretos todos tem respeito pois pra ganhar, não escolhe ladoSalve o Barretos, glória do Estado.")
+    
+# ------------------------------------------------------
+print("Inicio do codigo..")  
 
-print("Inicio do codigo..")    
+# chamando uma função (mensagem_boas_vindas())
+mensagem_boas_vindas()
+print("apos função")
+print("-"*20)
+
+cantar_musica()
+print("-"*20)
+cantar_musica()
+print("-"*20)
+cantar_musica()
+print("-"*20)
---
+++ parametros.py
@@ -0,0 +1,6 @@
+def calcular_idade(ano_nascimento):
+    print("Ano de Nascimento é:", ano_nascimento)
+    idade = 1993 - ano_nascimento
+    print("Ano de nascimento é:", idade)
+print("chamando a função")    
+calcular_idade()
